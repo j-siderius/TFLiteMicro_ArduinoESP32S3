ee270ce1c5e074e5d7665b6c64ce86f519d60813
Jannick <nick.rius@gmail.com>
2025-01-27 21:54:27 +0100

    Fixed HelloWorld example Arduino compatibility and removed redundant classes

--- examples/hello_world/hello_world.ino
+++ examples/hello_world/hello_world.ino
@@ -14,13 +14,12 @@ limitations under the License.
 ==============================================================================*/
 
 
-#include "src/tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "src/tensorflow/lite/micro/micro_interpreter.h"
-#include "src/tensorflow/lite/micro/system_setup.h"
-#include "src/tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
 
 #include "model.h"
-#include "output_handler.h"
 
 // Globals, used for compatibility with Arduino-style sketches.
 namespace {
@@ -30,7 +29,16 @@ TfLiteTensor* input = nullptr;
 TfLiteTensor* output = nullptr;
 int inference_count = 0;
 
+// This constant represents the range of x values our model was trained on,
+// which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional
+// libraries.
 const float kXrange = 2.f * 3.14159265359f;
+// This constant determines the number of inferences to perform across the range
+// of x values defined above. Since each inference takes time, the higher this
+// number, the more time it will take to run through the entire range. The value
+// of this constant can be tuned so that one full cycle takes a desired amount
+// of time. Since different devices take different amounts of time to perform
+// inference, this value should be defined per-device.
 const int kInferencesPerCycle = 20;
 
 constexpr int kTensorArenaSize = 2000;
@@ -102,14 +110,15 @@ void loop() {
   // Dequantize the output from integer to floating-point
   float y = (y_quantized - output->params.zero_point) * output->params.scale;
 
-  // Output the results. A custom HandleOutput function can be implemented
-  // for each supported hardware target.
-  HandleOutput(x, y);
+  // Log the current X and output Y
+  MicroPrintf("x_value:%f,y_value:%f", static_cast<double>(x),
+              static_cast<double>(y));
 
   // Increment the inference_counter, and reset it if we have reached
   // the total number per cycle
   inference_count += 1;
   if (inference_count >= kInferencesPerCycle) inference_count = 0;
 
-  delay(500);
+  // trigger one inference every 100ms
+  delay(100);
 }
